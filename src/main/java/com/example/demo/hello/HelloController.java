c0bdc49738a003c2dd332c8e1465e2a19243921c
repenguin22 <devSomaPro@gmail.com>
2022-05-14 19:02:36 +0900

    Fix: テストの追加とコンポーネントの移動

--- src/main/java/com/example/demo/hello/HelloController.java
+++ src/main/java/com/example/demo/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("hello")
-    public String hello() {
-        return "hello";
-    }
+  @GetMapping("hello")
+  public String hello() {
+    return "hello";
+  }
 }
